f30d7c52e0f9015617e31c4e9c97d5456f5260be
abdr9 <abiabdullahinnshaalloh@gmail.com>
2024-08-11 04:46:24 +0600

    Readme has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
               body: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
+                  children: [
                     const Text(
                       'You have pushed the button this many times:',
                     ),
